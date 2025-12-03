f240cf277d8b62f27e1512f746dd31e9512806b2
t4corun <VictorN076@gmail.com>
2025-12-02 20:20:36 -0600

    removed the Enum. - I swear my last zmk-config repo built

--- config/include/t4corun.h
+++ config/include/t4corun.h
@@ -1,16 +1,9 @@
 #pragma once
 
 // put the default base layers first
-enum layers {
-    _QWERTY = 0,
-    DEFAULT_LAYER = 0,
-    _COLEMAK_DH,
-    _GAME,
-    _NAVIGATION,
-    _NUMBER,
-    _SYMBOL,
-    _FUNCTION
-};
+#define _QWERTY     0
+#define _NUMBER     1
+#define _NAVIGATION 2
 
 //                           xxxxxxxxxxxxxxx|xxxxxxxxxxxxxxx|xxxxxxxxxxxxxxx|xxxxxxxxxxxxxxx 
 #define HRML(k1, k2, k3, k4) &hm LGUI k1     &hm LALT k2     &hm LCTRL k3    &hm LSHFT k4
