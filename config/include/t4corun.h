20a14a7f528bf4a849c6111d7547e09722502a88
t4corun <VictorN076@gmail.com>
2025-12-02 20:28:14 -0600

    Fixing my keycodes - used hm which is wrong - used mt instead of lt

--- config/include/t4corun.h
+++ config/include/t4corun.h
@@ -6,12 +6,12 @@
 #define _NAVIGATION 2
 
 //                           xxxxxxxxxxxxxxx|xxxxxxxxxxxxxxx|xxxxxxxxxxxxxxx|xxxxxxxxxxxxxxx 
-#define HRML(k1, k2, k3, k4) &hm LGUI k1     &hm LALT k2     &hm LCTRL k3    &hm LSHFT k4
-#define HRMR(k1, k2, k3, k4) &hm RSHFT k1    &hm RCTRL k2    &hm RALT k3     &hm RGUI  k4
+#define HRML(k1, k2, k3, k4) &mt LGUI k1     &mt LALT k2     &mt LCTRL k3    &mt LSHFT k4
+#define HRMR(k1, k2, k3, k4) &mt RSHFT k1    &mt RCTRL k2    &mt RALT k3     &mt RGUI  k4
 
 // layer changes
-#define NUM                  &mt _NUMBER
-#define NAV                  &mt _NAVIGATION
+#define NUM                  &lt _NUMBER
+#define NAV                  &lt _NAVIGATION
 
 //Behavior Configuration
 #define TAPPING_TERM 200
