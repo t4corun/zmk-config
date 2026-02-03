e3ee1a3a991086851c2bbe2f5af5de035875f577
t4corun <VictorN076@gmail.com>
2026-02-02 21:25:03 -0600

    Testing to see if the keymap works before doing combos

--- config/t4corun.h
+++ config/t4corun.h
@@ -17,17 +17,27 @@
 #define QUICK_TAP_TERM        150
 #define PRIOR_IDLE_TERM       100
 
-// layer changes
-#define NUMBER         &mo _NUMBER
-#define NAVIGATION     &mo _NAVIGATION
-#define FUNCTION       &mo _FUNCTION
+#define HRM_TAPPING_TERM      280
+#define HRM_QUICK_TAP_TERM    175
+#define HRM_PRIOR_IDLE_TERM   130
 
-#define QWERTY         &to _QWERTY
-#define GRAPHITE       &to _GRAPHITE
+#define COMBO_TIMEOUT         50
+
+// homerow mods
+#define HRML(k1, k2, k3, k4) &hml LGUI k1   &hml LALT k2   &hml LCTRL k3  &hml LSHFT k4
+#define HRMR(k1, k2, k3, k4) &hmr RSHFT k1  &hmr RCTRL k2  &hmr RALT k3   &hmr RGUI  k4
+
+// layer changes
+#define NUMBER               &mo _NUMBER
+#define NAVIGATION           &mo _NAVIGATION
+#define FUNCTION             &mo _FUNCTION
+
+#define QWERTY               &to _QWERTY
+#define GRAPHITE             &to _GRAPHITE
 
 // navigation
-#define NEXT_TAB       &kp LC(TAB)
-#define PREV_TAB       &kp LS(LC(TAB))
+#define NEXT_TAB             &kp LC(TAB)
+#define PREV_TAB             &kp LS(LC(TAB))
 
 // key overrides
 #define KO_COMMA             &ht LPAR      COMMA
@@ -102,6 +112,15 @@
         bindings = <HOLD_MACRO>, <&kp>;            \
     };
 
+#define COMBO_MACRO(NAME, POSITIONS, BINDING)      \
+    NAME: NAME {                                   \
+        compatible = "zmk,combos";                 \
+        timeout-ms = <COMBO_TIMEOUT>               \
+        key-positions = <POSITIONS>                \
+        bindings = <&kp>                           \
+    }
+
+
 
 // The 0 is required because we are hard coding the macro but the behavior
 // still wants two parameters
@@ -111,3 +130,66 @@
 #define MACRO_PAR  &ht_par 0 LPAR
 #define MACRO_SQUO &ht_sqt 0 SQT
 #define MACRO_DQUO &ht_dqt 0 DQT
+
+
+
+/*
+ * Position definition for timerless home row mods
+ *
+ *   ╭─────────────────────────┬─────────────────────────╮
+ *   │     LT4 LT3 LT2 LT1 LT0 │ RT0 RT1 RT2 RT3 RT4     │
+ *   │     LM4 LM3 LM2 LM1 LM0 │ RM0 RM1 RM2 RM3 RM4     │
+ *   │ LB5 LB4 LB3 LB2 LB1 LB0 │ RB0 RB1 RB2 RB3 RB4 RB5 │
+ *   ╰───────────╮ LH2 LH1 LH0 │ RH0 RH1 RH2 ╭───────────╯
+ *               ╰─────────────┴─────────────╯
+ */
+
+#define LT4 0
+#define LT3 1
+#define LT2 2
+#define LT1 3
+#define LT0 4
+
+#define RT0 5
+#define RT1 6
+#define RT2 7
+#define RT3 8
+#define RT4 9
+
+#define LM4 10
+#define LM3 11
+#define LM2 12
+#define LM1 13
+#define LM0 14
+
+#define RM0 15
+#define RM1 16
+#define RM2 17
+#define RM3 18
+#define RM4 19
+
+#define LB5 20
+#define LB4 21
+#define LB3 22
+#define LB2 23
+#define LB1 24
+#define LB0 25
+
+#define RB0 26
+#define RB1 27
+#define RB2 28
+#define RB3 29
+#define RB4 30
+#define RB5 31
+
+#define LH2 32
+#define LH1 33
+#define LH0 34
+
+#define RH0 35
+#define RH1 36
+#define RH2 37
+
+#define KEYS_L LT4 LT3 LT2 LT1 LT0 LM4 LM3 LM2 LM1 LM0 LB5 LB4 LB3 LB2 LB1 LB0
+#define KEYS_R RT0 RT1 RT2 RT3 RT4 RM0 RM1 RM2 RM3 RM4 RB0 RB1 RB2 RB3 RB4 RB5
+#define THUMBS LH2 LH1 LH0 RH0 RH1 RH2
